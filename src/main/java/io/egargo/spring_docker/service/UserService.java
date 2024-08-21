86ee5081f00c579b67acdf5d869fe3a2b2daa9f3
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-08-21 17:07:39 +0800

    refactor: refactor to more readable

--- src/main/java/io/egargo/spring_docker/service/UserService.java
+++ src/main/java/io/egargo/spring_docker/service/UserService.java
@@ -1,12 +1,20 @@
 package io.egargo.spring_docker.service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
+import java.io.File;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.protobuf.Option;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
@@ -25,62 +33,73 @@ public class UserService {
 	@Autowired
 	UserDTOMapper userDTOMapper;
 
-	public Iterable<User> getAll() {
-		return userRepository.findAll();
+	public List<UserDTO> getAll() {
+		return userRepository.getAllUsers();
 	}
 
-	public Optional<User> getById(Long id) {
-		return userRepository.findById(id);
+	public Optional<UserDTO> getById(Long id) {
+		return userRepository.getUserById(id);
 	}
 
 	public void createUser(User user) {
 		userRepository.save(user);
 	}
 
-	public void updateUser(Long id, UserCreateDTO userCreateDTO) {
+	@Transactional
+	public Integer updateUser(Long id, UserCreateDTO userCreateDTO) {
 		Optional<User> user = userRepository.findById(id);
 
-		if (user.isPresent()) {
-			UserUpdateDTO userUpdateDTO = new UserUpdateDTO();
-
-			userUpdateDTO.setDateRegistered(user.get().getDateRegistered());
-
-			if (userCreateDTO.firstName == null)
-				userUpdateDTO.setFirstName(userCreateDTO.firstName);
-			else
-				userUpdateDTO.setFirstName(user.get().getFirstName());
-
-			if (userCreateDTO.middleName == null)
-				userUpdateDTO.setMiddleName(userCreateDTO.middleName);
-			else
-				userUpdateDTO.setMiddleName(user.get().getMiddleName());
-
-			if (userCreateDTO.lastName == null)
-				userUpdateDTO.setLastName(userCreateDTO.lastName);
-			else
-				userUpdateDTO.setLastName(user.get().getLastName());
-
-			if (userCreateDTO.userName == null)
-				userUpdateDTO.setUserName(userCreateDTO.userName);
-			else
-				userUpdateDTO.setUserName(user.get().getUserName());
-
-			if (userCreateDTO.email == null)
-				userUpdateDTO.setEmail(userCreateDTO.email);
-			else
-				userUpdateDTO.setEmail(user.get().getEmail());
-
-			if (userCreateDTO.password == null)
-				userUpdateDTO.setPassword(userCreateDTO.password);
-			else
-				userUpdateDTO.setPassword(user.get().getPassword());
-
-			User up = userDTOMapper.userCreateDTO(userCreateDTO);
-			up.setId(user.get().getId());
-			userRepository.save(up);
-		} else {
-			System.out.println("Error");
+		if (!user.isPresent()) {
+			return 1;
 		}
+
+		userCreateDTO.password = BCrypt.withDefaults().hashToString(12, userCreateDTO.password.toCharArray());
+		userRepository.updateUser(id, userCreateDTO);
+		return 0;
+
+		// Optional<User> user = userRepository.findById(id);
+		//
+		// if (user.isPresent()) {
+		// UserUpdateDTO userUpdateDTO = new UserUpdateDTO();
+		//
+		// userUpdateDTO.setDateRegistered(user.get().getDateRegistered());
+		//
+		// if (userCreateDTO.firstName == null)
+		// userUpdateDTO.setFirstName(userCreateDTO.firstName);
+		// else
+		// userUpdateDTO.setFirstName(user.get().getFirstName());
+		//
+		// if (userCreateDTO.middleName == null)
+		// userUpdateDTO.setMiddleName(userCreateDTO.middleName);
+		// else
+		// userUpdateDTO.setMiddleName(user.get().getMiddleName());
+		//
+		// if (userCreateDTO.lastName == null)
+		// userUpdateDTO.setLastName(userCreateDTO.lastName);
+		// else
+		// userUpdateDTO.setLastName(user.get().getLastName());
+		//
+		// if (userCreateDTO.userName == null)
+		// userUpdateDTO.setUserName(userCreateDTO.userName);
+		// else
+		// userUpdateDTO.setUserName(user.get().getUserName());
+		//
+		// if (userCreateDTO.email == null)
+		// userUpdateDTO.setEmail(userCreateDTO.email);
+		// else
+		// userUpdateDTO.setEmail(user.get().getEmail());
+		//
+		// if (userCreateDTO.password == null)
+		// userUpdateDTO.setPassword(userCreateDTO.password);
+		// else
+		// userUpdateDTO.setPassword(user.get().getPassword());
+		//
+		// User up = userDTOMapper.userCreateDTO(userCreateDTO);
+		// up.setId(user.get().getId());
+		// userRepository.save(up);
+		// } else {
+		// System.out.println("Error");
+		// }
 	}
 
 	public void deleteUser(Long id) {
