a8a988bbe9f2149b07a2a3059bf0befe2146aa5e
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-08-21 17:06:44 +0800

    feat: add JPQL

--- src/main/java/io/egargo/spring_docker/repository/UserRepository.java
+++ src/main/java/io/egargo/spring_docker/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package io.egargo.spring_docker.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -12,23 +13,30 @@ import io.egargo.spring_docker.dto.UserDTO;
 import io.egargo.spring_docker.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	// @Modifying
-	// @Query("UPDATE User u " +
-	// "SET u.firstName=:#{#user.getName()}, " +
-	// " u.middleName=:#{#user.getMiddleName()}, " +
-	// " u.lastName=:#{#user.getLastName()}, " +
-	// " u.userName=:#{#user.getUsername()}, " +
-	// " u.email=:#{#user.getEmail()}, " +
-	// " u.password=:#{#user.getPassword()} " +
-	// "WHERE u.id=:#{#user.getId()}")
-	// void updateUser(@Param("user") UserCreateDTO user);
+	@Modifying
+	@Query("UPDATE User u " +
+			"SET u.firstName=:#{#user.firstName}, " +
+			"	u.middleName=:#{#user.middleName}, " +
+			"	u.lastName=:#{#user.lastName}, " +
+			"	u.userName=:#{#user.userName}, " +
+			"	u.email=:#{#user.email}, " +
+			"	u.password=:#{#user.password} " +
+			"WHERE u.id=:id")
+	Integer updateUser(@Param("id") Long id, @Param("user") UserCreateDTO user);
 
-	// @Query("SELECT " +
-	// // "u "+
-	// "u.firstName, u.middleName, u.lastName, u.userName, u.email " +
-	// // "u.getFirstName(), u.getMiddleName(), u.getLastName(), u.getUserName(),
-	// // u.getEmail() " +
-	// "FROM User u " +
-	// "ORDER BY id")
-	// List<UserDTO> findAllUsersCustom();
+	@Query("SELECT " +
+			"new io.egargo.spring_docker.dto.UserDTO(" +
+			"	u.id, u.firstName, u.middleName, u.lastName, u.userName, u.email" +
+			") " +
+			"FROM User u " +
+			"ORDER BY u.id")
+	List<UserDTO> getAllUsers();
+
+	@Query("SELECT " +
+			"new io.egargo.spring_docker.dto.UserDTO(" +
+			"	u.id, u.firstName, u.middleName, u.lastName, u.userName, u.email" +
+			") " +
+			"FROM User u " +
+			"WHERE u.id = :id")
+	Optional<UserDTO> getUserById(@Param("id") Long id);
 }
