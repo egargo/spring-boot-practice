29e1919d539908b3c557e127ebf2f5e2e833febc
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-08-21 10:00:01 +0800

    refactor: change patch to put

--- src/main/java/io/egargo/spring_docker/controller/UserController.java
+++ src/main/java/io/egargo/spring_docker/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -58,7 +58,7 @@ public class UserController {
 		}
 	}
 
-	@PatchMapping(value = { "", "/" }, consumes = "application/json", produces = "application/json")
+	@PutMapping(value = { "", "/" }, consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> updateUser(@RequestParam Long id, @RequestBody UserCreateDTO userCreateDTO) {
 		try {
 			userService.updateUser(id, userCreateDTO);
