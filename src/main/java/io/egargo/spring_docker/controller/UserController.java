0add98521a855d2656b82cfe73cd4df13e7e0fd5
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-08-27 11:06:30 +0800

    refactor: change response format to json

--- src/main/java/io/egargo/spring_docker/controller/UserController.java
+++ src/main/java/io/egargo/spring_docker/controller/UserController.java
@@ -1,5 +1,7 @@
 package io.egargo.spring_docker.controller;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,7 @@ import io.egargo.spring_docker.dto.UserCreateDTO;
 import io.egargo.spring_docker.entity.User;
 import io.egargo.spring_docker.mapper.UserDTOMapper;
 import io.egargo.spring_docker.service.UserService;
+import io.egargo.spring_docker.utils.UtilEnumResult;
 
 @RestController
 @RequestMapping("/api/users")
@@ -30,18 +33,22 @@ public class UserController {
 	@GetMapping(value = { "", "/" }, produces = "application/json")
 	public ResponseEntity<?> getAllUser() {
 		try {
-			return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+			return new ResponseEntity<>(Collections.singletonMap("data", userService.getAll()),
+					HttpStatus.CREATED);
 		} catch (Exception e) {
-			return new ResponseEntity<>("An unexpected error occurred: " + e, HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Collections.singletonMap("message", "An unexpected error occurred"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping(value = { "/{id}" }, produces = "application/json")
 	public ResponseEntity<?> getUserById(@PathVariable Long id) {
 		try {
-			return new ResponseEntity<>(userService.getById(id), HttpStatus.OK);
+			return new ResponseEntity<>(Collections.singletonMap("data", userService.getById(id)),
+					HttpStatus.CREATED);
 		} catch (Exception e) {
-			return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Collections.singletonMap("message", "An unexpected error occurred"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
@@ -51,37 +58,43 @@ public class UserController {
 			User user = userDTOMapper.userCreateDTO(userCreateDTO);
 			userService.createUser(user);
 
-			return new ResponseEntity<>("Successfully created user", HttpStatus.CREATED);
+			return new ResponseEntity<>(Collections.singletonMap("message", "Successfully created user"),
+					HttpStatus.CREATED);
 		} catch (Exception e) {
-			return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Collections.singletonMap("message", "An unexpected error occurred"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@PutMapping(value = { "", "/" }, consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> updateUser(@RequestParam Long id, @RequestBody UserCreateDTO userCreateDTO) {
 		try {
-			Integer u = userService.updateUser(id, userCreateDTO);
+			UtilEnumResult u = userService.updateUser(id, userCreateDTO);
 
 			switch (u) {
-				case 0:
-					return new ResponseEntity<>("Successfully updated user", HttpStatus.OK);
+				case Error:
+					return new ResponseEntity<>(Collections.singletonMap("message", "Failed to update user"),
+							HttpStatus.INTERNAL_SERVER_ERROR);
 				default:
-					return new ResponseEntity<>("Failed to update user", HttpStatus.INTERNAL_SERVER_ERROR);
+					return new ResponseEntity<>(Collections.singletonMap("message", "Successfully updated user"),
+							HttpStatus.OK);
 			}
 
 		} catch (Exception e) {
-			System.out.println("Error: " + e);
-			return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Collections.singletonMap("message", "An unexpected error occurred"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
-	@DeleteMapping(value = { "", "/{id}" }, consumes = "application/json", produces = "application/json")
+	@DeleteMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<?> deleteUser(@PathVariable Long id) {
 		try {
 			userService.deleteUser(id);
-			return new ResponseEntity<>("Successfully deleted user", HttpStatus.OK);
+			return new ResponseEntity<>(Collections.singletonMap("message", "Successfully deleted user"),
+					HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Collections.singletonMap("message", "An unexpected error occurred"),
+					HttpStatus.OK);
 		}
 	}
 }
