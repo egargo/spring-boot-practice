ce81e56accd654b9715c968b79fb58ca62897af3
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-08-27 11:05:48 +0800

    refactor: remove @RestController annotation

--- src/main/java/io/egargo/spring_docker/SpringDockerApplication.java
+++ src/main/java/io/egargo/spring_docker/SpringDockerApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableJpaRepositories(basePackages = { "io.egargo.spring_docker.repository" })
 @EnableAutoConfiguration
 @EntityScan("io.egargo.spring_docker.entity")
