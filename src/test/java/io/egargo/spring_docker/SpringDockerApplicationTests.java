79b19470986dbbbe4d1ba44e8eb0b46d2f8a5851
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-08-29 14:26:46 +0800

    test: add application-test.properties and test methods

--- src/test/java/io/egargo/spring_docker/SpringDockerApplicationTests.java
+++ src/test/java/io/egargo/spring_docker/SpringDockerApplicationTests.java
@@ -1,13 +1,76 @@
 package io.egargo.spring_docker;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import io.egargo.spring_docker.model.Savings;
+import io.egargo.spring_docker.model.User;
+import io.egargo.spring_docker.repository.SavingsRepository;
+import io.egargo.spring_docker.repository.UserRepository;
 
 @SpringBootTest
 class SpringDockerApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private SavingsRepository savingsRepository;
+
 	@Test
-	void contextLoads() {
+	public void createUser() {
+		double randomId = Math.random();
+
+		User user = new User();
+		user.setFirstName("Test");
+		user.setMiddleName("Test");
+		user.setLastName("Test");
+		user.setUserName("test" + randomId);
+		user.setEmail("test" + randomId + "@test.test");
+		user.setPassword("Password");
+		user.setDateRegistered(LocalDateTime.now());
+
+		userRepository.save(user);
+
+		Assertions.assertNotNull(user.getId());
 	}
 
+	@Test
+	public void createUserAndBalance() {
+		double randomId = Math.random();
+
+		User newUser = new User();
+		newUser.setFirstName("Test");
+		newUser.setMiddleName("Test");
+		newUser.setLastName("Test");
+		newUser.setUserName("test" + randomId);
+		newUser.setEmail("test" + randomId + "@test.test");
+		newUser.setPassword("Password");
+		newUser.setDateRegistered(LocalDateTime.now());
+
+		userRepository.save(newUser);
+		Long id = newUser.getId();
+
+		User user = userRepository.getById(id);
+		Savings savings = new Savings();
+
+		double randomBal = Math.random();
+
+		savings.setUser(user);
+		savings.setBalance(randomBal);
+
+		savingsRepository.save(savings);
+
+		Assertions.assertNotNull(id);
+		Assertions.assertNotNull(savings.getId());
+	}
 }
