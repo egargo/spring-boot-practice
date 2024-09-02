594edf94a94b407a33baaecdf789b0e9b5a87d70
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-02 15:15:33 +0800

    feat: add jwt authentication filter

---
+++ src/main/java/io/egargo/spring_docker/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package io.egargo.spring_docker.configuration;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.egargo.spring_docker.model.JwtClaim;
+import io.egargo.spring_docker.utils.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+	private final JwtUtil jwtUtil;
+
+	public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		String authorizationHeader = httpServletRequest.getHeader("Authorization");
+		String refreshToken = httpServletRequest.getHeader("refreshToken");
+
+		ObjectMapper map = new ObjectMapper();
+		map.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			String token = authorizationHeader.split("Bearer ")[2].trim();
+			String username = map.convertValue(jwtUtil.extractAllClaims(token).get("data"), JwtClaim.class)
+					.getUsername();
+
+			if (username != null & jwtUtil.verifyToken(token) == true) {
+				SecurityContextHolder.getContext()
+						.setAuthentication(new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>()));
+			}
+		}
+
+		chain.doFilter(request, response);
+	}
+}
