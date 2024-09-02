08a4d2224a041390655e2e13b6b57d9cd8625f17
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-02 11:28:22 +0800

    feat: add @Service annotation

--- src/main/java/io/egargo/spring_docker/utils/Password.java
+++ src/main/java/io/egargo/spring_docker/utils/Password.java
@@ -1,8 +1,11 @@
 package io.egargo.spring_docker.utils;
 
+import org.springframework.stereotype.Service;
+
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import lombok.NoArgsConstructor;
 
+@Service
 @NoArgsConstructor
 public class Password {
 	public String hash(String password) {
