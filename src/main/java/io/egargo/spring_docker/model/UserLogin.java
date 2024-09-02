1bc2d541ef9284dda4454e8f4122ff88f7b8bf30
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-02 15:17:07 +0800

    feat: add user login dto

---
+++ src/main/java/io/egargo/spring_docker/model/UserLogin.java
@@ -0,0 +1,11 @@
+package io.egargo.spring_docker.model;
+
+public class UserLogin {
+	public String userName;
+	public String password;
+
+	public UserLogin(String userName, String password) {
+		this.userName = userName;
+		this.password = password;
+	}
+}
