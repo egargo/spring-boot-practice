1dc1ad16750dba3bb65b930f7b2f85cd6d3e48fe
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-05 09:23:40 +0800

    refactor: comment out kafka configuration

--- src/main/java/io/egargo/spring_docker/configuration/KafkaMessageProducer.java
+++ src/main/java/io/egargo/spring_docker/configuration/KafkaMessageProducer.java
@@ -1,15 +1,15 @@
-package io.egargo.spring_docker.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaMessageProducer {
-	@Autowired
-	private KafkaTemplate<String, String> kafkaTemplate;
-
-	public void sendMessage(String topic, String message) {
-		kafkaTemplate.send(topic, message);
-	}
-}
+// package io.egargo.spring_docker.configuration;
+//
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.kafka.core.KafkaTemplate;
+// import org.springframework.stereotype.Component;
+//
+// @Component
+// public class KafkaMessageProducer {
+// @Autowired
+// private KafkaTemplate<String, String> kafkaTemplate;
+//
+// public void sendMessage(String topic, String message) {
+// kafkaTemplate.send(topic, message);
+// }
+// }
--- src/main/java/io/egargo/spring_docker/configuration/MessageConsumer.java
+++ src/main/java/io/egargo/spring_docker/configuration/MessageConsumer.java
@@ -1,12 +1,12 @@
-package io.egargo.spring_docker.configuration;
-
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class MessageConsumer {
-	@KafkaListener(topics = "my-topic", groupId = "my-group-id")
-	public void listen(String message) {
-		System.out.println("Received message: " + message);
-	}
-}
+// package io.egargo.spring_docker.configuration;
+//
+// import org.springframework.kafka.annotation.KafkaListener;
+// import org.springframework.stereotype.Component;
+//
+// @Component
+// public class MessageConsumer {
+// 	@KafkaListener(topics = "my-topic", groupId = "my-group-id")
+// 	public void listen(String message) {
+// 		System.out.println("Received message: " + message);
+// 	}
+// }
