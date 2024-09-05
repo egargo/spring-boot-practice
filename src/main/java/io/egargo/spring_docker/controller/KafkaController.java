e4c22cebd26cd3728e72c011ec534b007075a509
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-05 09:24:18 +0800

    refactor: comment out kafka controller

--- src/main/java/io/egargo/spring_docker/controller/KafkaController.java
+++ src/main/java/io/egargo/spring_docker/controller/KafkaController.java
@@ -1,20 +1,20 @@
-package io.egargo.spring_docker.controller;
-
-import io.egargo.spring_docker.configuration.KafkaMessageProducer;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class KafkaController {
-	@Autowired
-	private KafkaMessageProducer kafkaMessageProducer;
-
-	@PostMapping("/send")
-	public String sendMessage(@RequestParam("message") String message) {
-		kafkaMessageProducer.sendMessage("my-topic", message);
-		return "Message sent: " + message;
-	}
-}
+// package io.egargo.spring_docker.controller;
+//
+// import io.egargo.spring_docker.configuration.KafkaMessageProducer;
+//
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// @RestController
+// public class KafkaController {
+// @Autowired
+// private KafkaMessageProducer kafkaMessageProducer;
+//
+// @PostMapping("/send")
+// public String sendMessage(@RequestParam("message") String message) {
+// kafkaMessageProducer.sendMessage("my-topic", message);
+// return "Message sent: " + message;
+// }
+// }
