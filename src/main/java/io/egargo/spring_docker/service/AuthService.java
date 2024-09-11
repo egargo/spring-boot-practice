cbf81fac1b321302b48cfbdf50a7c47de48627cb
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-11 17:45:50 +0800

    refactor: minor code refactor

--- src/main/java/io/egargo/spring_docker/service/AuthService.java
+++ src/main/java/io/egargo/spring_docker/service/AuthService.java
@@ -4,14 +4,15 @@ import java.util.HashMap;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
+import io.egargo.spring_docker.dto.UserCheckDTO;
 import io.egargo.spring_docker.model.UserLogin;
 import io.egargo.spring_docker.repository.UserRepository;
 import io.egargo.spring_docker.utils.JwtUtil;
 import io.egargo.spring_docker.utils.Password;
 
-@Controller
+@Service
 public class AuthService {
 	@Autowired
 	JwtUtil jwtUtil;
@@ -23,19 +24,17 @@ public class AuthService {
 	UserRepository userRepository;
 
 	public Optional<HashMap<String, String>> login(UserLogin userLogin) {
-		Optional<String> user = userRepository.checkUserLogin(userLogin.userName);
-		Optional<HashMap<String, String>> map = Optional.of(new HashMap<>());
-
-		if (user.isEmpty() || (!passwordUtil.verify(userLogin.password, user.get()).verified)) {
+		Optional<UserCheckDTO> user = userRepository.checkUserLogin(userLogin.userName);
+		if (user.isEmpty() || (!passwordUtil.verify(userLogin.password, user.get().password))) {
 			return Optional.empty();
 		}
 
-		final String accessToken = jwtUtil.generateAccessToken(userLogin);
-		final String refreshToken = jwtUtil.generateRefreshToken(userLogin);
+		final String accessToken = jwtUtil.generateAccessToken(user.get());
+		final String refreshToken = jwtUtil.generateRefreshToken(user.get());
 
+		Optional<HashMap<String, String>> map = Optional.of(new HashMap<>());
 		map.get().put("accessToken", accessToken);
 		map.get().put("refreshToken", refreshToken);
-
 		return map;
 	}
 }
