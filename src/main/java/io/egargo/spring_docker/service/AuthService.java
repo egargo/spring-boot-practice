701c889afacf389959ea95dba5cd31c1b17fc046
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-17 17:17:27 +0800

    refactor: AuthService

--- src/main/java/io/egargo/spring_docker/service/AuthService.java
+++ src/main/java/io/egargo/spring_docker/service/AuthService.java
@@ -4,13 +4,20 @@ import java.util.HashMap;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.egargo.spring_docker.dto.UserCheckDTO;
+import io.egargo.spring_docker.model.JwtClaim;
 import io.egargo.spring_docker.model.UserLogin;
 import io.egargo.spring_docker.repository.UserRepository;
 import io.egargo.spring_docker.utils.JwtUtil;
 import io.egargo.spring_docker.utils.Password;
+import java.util.Collections;
 
 @Service
 public class AuthService {
@@ -23,18 +30,41 @@ public class AuthService {
 	@Autowired
 	UserRepository userRepository;
 
-	public Optional<HashMap<String, String>> login(UserLogin userLogin) {
+	public ResponseEntity<?> login(UserLogin userLogin) {
 		Optional<UserCheckDTO> user = userRepository.checkUserLogin(userLogin.userName);
+
 		if (user.isEmpty() || (!passwordUtil.verify(userLogin.password, user.get().password))) {
-			return Optional.empty();
+			return new ResponseEntity<>(Collections.singletonMap("message", "Incorrect username and/or password"),
+					HttpStatus.NOT_FOUND);
 		}
 
-		final String accessToken = jwtUtil.generateAccessToken(user.get());
-		final String refreshToken = jwtUtil.generateRefreshToken(user.get());
+		final JwtClaim claim = new JwtClaim();
+		claim.setUserName(user.get().userName);
+		claim.setEmail(user.get().email);
+		claim.setRole(user.get().role.toString());
+
+		final String accessToken = jwtUtil.generateAccessToken(claim);
+		final String refreshToken = jwtUtil.generateRefreshToken(claim);
 
 		Optional<HashMap<String, String>> map = Optional.of(new HashMap<>());
 		map.get().put("accessToken", accessToken);
 		map.get().put("refreshToken", refreshToken);
-		return map;
+
+		return new ResponseEntity<>(Collections.singletonMap("data", map), HttpStatus.OK);
+	}
+
+	public ResponseEntity<?> refresh(String refreshToken) {
+		if (!jwtUtil.verifyToken(refreshToken)) {
+			return new ResponseEntity<>(Collections.singletonMap("message", "Access token is expired and/or invalid"),
+					HttpStatus.UNAUTHORIZED);
+		}
+
+		ObjectMapper map = new ObjectMapper();
+		map.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+		JwtClaim claim = map.convertValue(jwtUtil.extractAllClaims(refreshToken).get("data"), JwtClaim.class);
+
+		return new ResponseEntity<>(Collections.singletonMap("accessToken", jwtUtil.generateAccessToken(claim)),
+				HttpStatus.OK);
 	}
 }
