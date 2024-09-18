b93355ef1c6517ecc58ac29827c702d481c4a977
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-18 09:58:26 +0800

    feat: add Set<GrantedAuthority> to SecurityContextHolder

--- src/main/java/io/egargo/spring_docker/configuration/JwtAuthenticationFilter.java
+++ src/main/java/io/egargo/spring_docker/configuration/JwtAuthenticationFilter.java
@@ -2,9 +2,15 @@ package io.egargo.spring_docker.configuration;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
@@ -37,12 +43,19 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
 			String token = authorizationHeader.split("Bearer ")[2].trim();
-			String username = map.convertValue(jwtUtil.extractAllClaims(token).get("data"), JwtClaim.class)
-					.getUserName();
+			JwtClaim claim = map.convertValue(jwtUtil.extractAllClaims(token).get("data"), JwtClaim.class);
+			String username = claim.getUserName();
+			String role = claim.getRole();
 
-			if (username != null & jwtUtil.verifyToken(token) == true) {
+			if (username != null && role != null && jwtUtil.verifyToken(token)) {
+				Set<GrantedAuthority> authority = new HashSet<>();
+				authority.add(new SimpleGrantedAuthority(role));
+
+				BCryptPasswordEncoder bEncoder = new BCryptPasswordEncoder();
+
 				SecurityContextHolder.getContext()
-						.setAuthentication(new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>()));
+						.setAuthentication(
+								new UsernamePasswordAuthenticationToken(username, null, authority));
 			}
 		}
 
