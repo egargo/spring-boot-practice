8844d81b32e2718f58fb458c476c8f8c2c8c350c
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-24 14:42:17 +0800

    feat: add signup endpoint

--- src/main/java/io/egargo/spring_docker/controller/AuthController.java
+++ src/main/java/io/egargo/spring_docker/controller/AuthController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.egargo.spring_docker.dto.UserCreateDTO;
+import io.egargo.spring_docker.mapper.UserDTOMapper;
+import io.egargo.spring_docker.model.User;
 import io.egargo.spring_docker.model.UserLogin;
 import io.egargo.spring_docker.service.AuthService;
 import io.egargo.spring_docker.utils.JwtUtil;
@@ -24,6 +27,21 @@ public class AuthController {
 
 	@Autowired
 	JwtUtil jwtUtil;
+
+	@Autowired
+	UserDTOMapper userDTOMapper;
+
+	@PostMapping(value = "/signup", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> singup(@RequestBody UserCreateDTO userCreateDTO) {
+		try {
+			User user = userDTOMapper.userCreateDTO(userCreateDTO);
+			return authService.signup(user);
+		} catch (Exception e) {
+			return new ResponseEntity<>(
+					Collections.singletonMap("message", "An unexpected error occurred"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 	@PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> login(@RequestBody UserLogin userLogin) {
