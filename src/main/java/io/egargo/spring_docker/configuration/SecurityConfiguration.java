3ac244d5cf1355474706472118ef8c2d396bc206
Clint Egargo <67180945+egargo@users.noreply.github.com>
2024-09-24 14:41:44 +0800

    refactor: update create account route

--- src/main/java/io/egargo/spring_docker/configuration/SecurityConfiguration.java
+++ src/main/java/io/egargo/spring_docker/configuration/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
 				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/api/auth/login").permitAll()
-						.requestMatchers("/api/users/create").permitAll()
+						.requestMatchers("/api/auth/signup").permitAll()
 						.anyRequest().authenticated())
 				.addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
 				.build();
